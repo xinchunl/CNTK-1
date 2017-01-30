e80139d9f1c5dfceda6365ff2729c96f1e8656f4
Ivan Rodriguez <t-ivrodr@microsoft.com>
2017-01-30 11:50:21 +0100

    Squeezing v2 library tests into one project

--- Tests/UnitTests/V2LibraryTests/FunctionTests.cpp
+++ Tests/UnitTests/V2LibraryTests/FunctionTests.cpp
@@ -847,8 +847,14 @@ BOOST_AUTO_TEST_CASE(TransposeInGPU)
 BOOST_AUTO_TEST_CASE(OutputVariableNameInCPU)
 {
     TestOuputVariableName(DeviceDescriptor::CPUDevice());
+}
+
+BOOST_AUTO_TEST_CASE(FunctionOutputs)
+{
     TestFunctionOutputs(DeviceDescriptor::CPUDevice());
 }
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
